a60b2f2ce17d04d95084190d3dfb0b7650608b16
CodeBucket234 <77819527+CodeBucket234@users.noreply.github.com>
2023-01-27 21:39:06 -0500

    Fixing Activity 3

--- ClassActivities/src/WeekThree/Activity3/Triangles.java
+++ ClassActivities/src/WeekThree/Activity3/Triangles.java
@@ -1,7 +1,19 @@
 package WeekThree.Activity3;
 //Importing scanner class
 import java.util.Scanner;
-
+//**********************************************************************************************************************
+// Activity 3: Triangles
+// Name: Blaine Bailey
+// Date of Submission: 1/27/2023
+//**********************************************************************************************************************
+// To use this program:
+// Click the green arrow, then enter a planet from our solar system. Our program is case-insensitive. If you enter a
+// planet from our solar system, then it will display a bunch of facts about the planet, and provide a link citation to
+// the website with the information. If you do not enter a planet from our solar system, then you will be given an
+// error message.
+//**********************************************************************************************************************
+// This program uses the Scanner class for input.
+//**********************************************************************************************************************
 public class Triangles {
     public static void main(String[] args) {
         //Creating scanner object
@@ -22,11 +34,11 @@ public class Triangles {
         else if(s1 == s2 && s1 == s3) {
             System.out.println("Your triangle is equilateral.");
         }
-        else if((s1 == s2 && s2 != s3) || (s1 != s2 && s2 == s3)) {
-            System.out.println("Your triangle is isosceles.");
+        else if(s1 != s2 && s2 != s3 && s1 != s3) {
+            System.out.println("Your triangle is scalene.");
         }
         else {
-            System.out.println("Your triangle is scalene.");
+            System.out.println("Your triangle is isosceles.");
         }
     }
 }
