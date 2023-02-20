e3d7c033247f5fa12b011a9b0e5a19c1038d8cfc
CodeBucket234 <77819527+CodeBucket234@users.noreply.github.com>
2023-02-19 21:43:23 -0500

    Finished Activity 14

--- overloaded/src/Main.java
+++ overloaded/src/Main.java
@@ -1,5 +1,46 @@
+//**********************************************************************************************************************
+// Activity 14: Overloaded Constructors and Methods
+// Name: Blaine Bailey
+// Date of Submission: 2/19/2023
+//**********************************************************************************************************************
+// To use this program, click the green arrow. This program uses the Pokemon class to create three pokemon using each of
+// the overloaded constructors. The secondary pokemon is used to test the overloaded method, which is the LevelUp
+// method. The secondary pokemon's starting stats are shown, then it is leveled up. The new stats for the secondary
+// pokemon are shown after leveling up once. Then the secondary pokemon is leveled up four times using the overloaded
+// method. The final stats are shown at the end.
+//**********************************************************************************************************************
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Creating three pokemon to call each of the overloaded constructors.
+        Pokemon primary = new Pokemon();
+        Pokemon secondary = new Pokemon("Pikachu");
+        Pokemon tertiary = new Pokemon("Lucario", 60, 6, 20);
+
+        //Showing the base stats of the secondary pokemon
+        System.out.printf("\nThe secondary pokemon's starting name is %s.\n", secondary.getName());
+        System.out.printf("The secondary pokemon's starting health is %d.\n", secondary.getHP());
+        System.out.printf("The secondary pokemon's starting level is %d.\n", secondary.getLVL());
+        System.out.printf("The secondary pokemon's starting defense is %d.\n", secondary.getDefense());
+
+        //Leveling up the secondary pokemon with the first instance of the overloaded method.
+        secondary.LevelUp();
+
+        //Showing the new stats of the secondary pokemon after leveling it up
+        System.out.println("\nAfter leveling up once, here is the new stats for the secondary pokemon:");
+        System.out.printf("The secondary pokemon's name is %s.\n", secondary.getName());
+        System.out.printf("The secondary pokemon's new health is %d.\n", secondary.getHP());
+        System.out.printf("The secondary pokemon's new level is %d.\n", secondary.getLVL());
+        System.out.printf("The secondary pokemon's new defense is %d.\n\n", secondary.getDefense());
+
+        //Using the other overloaded method to level up the secondary pokemon 4 times.
+        secondary.LevelUp(4);
+
+        //Showing the result of using both overloaded methods.
+        System.out.println("\nAfter leveling up four more times, here is the final stats for the secondary pokemon:");
+        System.out.printf("The secondary pokemon's ending name is %s.\n", secondary.getName());
+        System.out.printf("The secondary pokemon's ending health is %d.\n", secondary.getHP());
+        System.out.printf("The secondary pokemon's ending level is %d.\n", secondary.getLVL());
+        System.out.printf("The secondary pokemon's ending defense is %d.\n", secondary.getDefense());
     }
 }
--- overloaded/src/Pokemon.java
+++ overloaded/src/Pokemon.java
@@ -1,7 +1,25 @@
+//**********************************************************************************************************************
+// Activity 14: Overloaded Constructors and Methods
+// Name: Blaine Bailey
+// Date of Submission: 2/19/2023
+//**********************************************************************************************************************
+// This is the pokemon class. This class features three overloaded constructors, a default constructor that makes a
+// MissingNo. pokemon will 100 hp and defense at level 1. The overloaded constructor with only the name as the parameter
+// creates a base pokemon with the base stats of 35 hp and 10 defense at level 1 with the chosen name. The final
+// overloaded constructor creates a pokemon with all customized stats, such as a custom name, hp, level, and defense.
+// This program uses getter and setter methods. Finally, this program uses two overloaded LevelUp methods. The default
+// LevelUp method levels up the pokemon 1 time, adds 5 hp and 2 defense. The other LevelUp method levels up the pokemon
+// the specified number of times, and adds 5 hp for every level. This LevelUp method also adds 2 defense for every
+// level.
+//**********************************************************************************************************************
 public class Pokemon {
+    //Declaring instance variables
     String name;
     private int hp, level, defense;
 
+    //Overloaded constructors
+
+    //Default constructor creates "MissingNo" pokemon
     public Pokemon() {
         this.name = "MissingNo.";
         this.hp = 100;
@@ -9,6 +27,15 @@ public class Pokemon {
         this.defense = 100;
     }
 
+    //Creates base pokemon with chosen name
+    public Pokemon(String name) {
+        this.name = name;
+        this.hp = 35;
+        this.level = 1;
+        this.defense = 10;
+    }
+
+    //Creates pokemon with custom stats
     public Pokemon(String name, int hp, int lvl, int def) {
         this.name = name;
         this.hp = hp;
@@ -34,5 +61,35 @@ public class Pokemon {
     }
 
     //Setters
-    public void
+    public void rename(String newName) {
+        this.name = newName;
+    }
+
+    public void changeHP(int newHealth) {
+        this.hp = newHealth;
+    }
+
+    public void changeLVL(int newLVL) {
+        this.level = newLVL;
+    }
+
+    public void changeDefense(int newDef) {
+        this.defense = newDef;
+    }
+
+    //Overloaded method
+
+    //Levels up pokemon once
+    public void LevelUp() {
+        this.changeLVL(this.getLVL()+1);
+        this.changeHP(this.getHP()+5);
+        this.changeDefense(this.getDefense()+2);
+    }
+
+    //Levels up the pokemon the number of times specified in the argument
+    public void LevelUp(int newLevels) {
+        this.changeLVL(this.getLVL()+newLevels);
+        this.changeHP(this.getHP()+(newLevels*5));
+        this.changeDefense(this.getDefense()+(newLevels*2));
+    }
 }
