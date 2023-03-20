9445439f83f7ed011ae184b23e27d47befc36eb9
CodeBucket234 <77819527+CodeBucket234@users.noreply.github.com>
2023-03-20 00:33:33 -0400

    Finished Activity 24

--- bigOActivity/src/BigO.java
+++ bigOActivity/src/BigO.java
@@ -1,12 +1,31 @@
+//**********************************************************************************************************************
+// Names: Blaine Bailey and David Case
+// Activity: Big O
+// Date of Submission: 3/20/2023
+//**********************************************************************************************************************
+// The BigO class contains three methods. The printOnce method prints out the number that was passed into the function,
+// and runs in O(1) time. The printNTimes method prints out the phrase "This method runs in O(n) time." the number of
+// times passed into the function, and it runs in O(n) time. The printNSquaredTimes method prints out the phrase "This
+// method runs in O(n Squared) time." the number of times passed into the function squared, and it runs in O(n Squared)
+// time.
+//**********************************************************************************************************************
 public class BigO {
+    //Prints out the value passed into the method
+    //Runs in O(1) time
     public void printOnce(int num) {
         System.out.println(num);
     }
+
+    //Prints out a statement the number of times passed into the method
+    //Runs in O(n) time
     public void printNTimes(int times) {
         for(int i = 0; i < times; i++) {
             System.out.println("This method runs in O(n) time.");
         }
     }
+
+    //Prints out a statement the number of times passed into the method squared
+    //Runs in O(n Squared) times
     public void printNSquaredTimes(int times) {
         for(int i = 0; i < times; i++) {
             for(int j = 0; j < times; j++) {
--- bigOActivity/src/Test.java
+++ bigOActivity/src/Test.java
@@ -1,11 +1,26 @@
+//**********************************************************************************************************************
+// Names: Blaine Bailey and David Case
+// Activity: Big O
+// Date of Submission: 3/20/2023
+//**********************************************************************************************************************
+// This is the testing class for the BigO class. The program creates an instance of the BigO class, and then uses the
+// class to call each of the three methods. The number 6 is passed into the printOnce method, which prints out the
+// number 6 into the console. The number 4 is passed into the printNTimes method, which prints out "This method runs in
+// O(n) time." 4 times. The number 3 is passed into the printNSquaredTimes method, which prints out "This method runs in
+// O(n Squared) time." 9 times, because 3 squared is 9.
+//**********************************************************************************************************************
 public class Test {
     public static void main(String[] args) {
+        //Create instance of BigO class
         BigO o = new BigO();
 
+        //Call printOnce method with 6 as the argument
         o.printOnce(6);
         System.out.println();
+        //Call printNTimes method with 4 as the argument
         o.printNTimes(4);
         System.out.println();
+        //Call printNSquaredTimes method with 3 as the argument
         o.printNSquaredTimes(3);
     }
 }
