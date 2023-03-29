31efb5b09b0c4200a2c57f53854f9761f2301a36
CodeBucket234 <77819527+CodeBucket234@users.noreply.github.com>
2023-03-28 23:04:45 -0400

    Finished Activity 26

--- sortingActivity2/src/Main.java
+++ sortingActivity2/src/Main.java
@@ -1,5 +1,28 @@
+//**********************************************************************************************************************
+// Activity 26: Selection, Insertion, & Quick Sort
+// Name: Blaine Bailey
+// Date of Submission: 3/28/2023
+//**********************************************************************************************************************
+// This is the main class. The main class creates an instance of the sorting class. Then it calls the getArray method to
+// get the user to create an unsorted array with 5 integers. Then it shows the array the user created using the show
+// method. Then it uses the sortArray method to sort the array, and then it uses the show method to display the sorted
+// array.
+//**********************************************************************************************************************
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Create instance of Sorting class
+        Sorting selection = new Sorting();
+
+        //Get user to create array with 5 unsorted integers
+        int[] array = selection.getArray();
+
+        //Display the array the user created
+        System.out.println(selection.show(array));
+
+        //Sort the unsorted array
+        int[] sorted = selection.sortArray(array, 0);
+
+        //Display the sorted array
+        System.out.println(selection.show(sorted));
     }
 }
--- sortingActivity2/src/Sorting.java
+++ sortingActivity2/src/Sorting.java
@@ -1,7 +1,22 @@
+//Import Scanner class
 import java.util.Scanner;
+//**********************************************************************************************************************
+// Activity 26: Selection, Insertion, & Quick Sort
+// Name: Blaine Bailey
+// Date of Submission: 3/28/2023
+//**********************************************************************************************************************
+// This is the sorting class. This class features a getArray method that prompts the user for 5 unsorted integers and
+// returns an array with the 5 unsorted integers. The sortArray method sorts the array using selection sort. The
+// findMinIndex method helps the sortArray method to find the index value of the minimum integer for sorting. The show
+// method is used to display the arrays.
+//**********************************************************************************************************************
 public class Sorting {
+    //Creating scanner object for user input
     Scanner input = new Scanner(System.in);
+
+    //Prompts user to input 5 unsorted integers and returns the unsorted array
     public int[] getArray() {
+        //Prompt user for 5 unsorted integers
         System.out.print("Enter integer 1: ");
         int i1 = Integer.parseInt(input.nextLine());
         System.out.print("Enter integer 2: ");
@@ -13,12 +28,64 @@ public class Sorting {
         System.out.print("Enter integer 5: ");
         int i5 = Integer.parseInt(input.nextLine());
 
+        //Put the integers into an array
         int integers[] = {i1, i2, i3, i4, i5};
 
+        //Return the array
         return integers;
     }
 
-    public int[] sortArray() {
+    //Sorts the array using selection sort
+    public int[] sortArray(int[] array, int start) {
+        //Calling minIndex function to find the index with the minimum value
+        int minIndex = findMinIndex(array, start);
 
+        //Swapping the minimum value with the value of the starting index
+        int temp = array[start];
+        array[start] = array[minIndex];
+        array[minIndex] = temp;
+
+        //Only call sortArray recursively if there is more elements in the array
+        if(start < array.length - 1) {
+            sortArray(array, start + 1);
+        }
+
+        //Return the sorted array
+        return array;
+    }
+
+    //Helper function findMin
+    private int findMinIndex(int[] array, int start) {
+        //Start index of minimum value to be start index
+        int minIndex = start;
+
+        //Find the index of the minimum value
+        for(int i = start; i < array.length; i++) {
+            if(array[i] < array[minIndex]) {
+                minIndex = i;
+            }
+        }
+
+        //Return the index of the minimum value.
+        return minIndex;
+    }
+
+    //Method for displaying all values in the array
+    public String show(int[] array) {
+        //Create display string
+        String display = "";
+
+        //Fill display string with all values in the array
+        for(int i = 0; i < array.length; i++) {
+            if(i < array.length-1) {
+                display += array[i] + ", ";
+            }
+            else {
+                display += array[i];
+            }
+        }
+
+        //Return the display string
+        return display;
     }
 }
